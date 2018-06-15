6ed1cbcd85c2eb00cfe66d2f03723f9fb470e08c
Hannes Mehnert <hannes@mehnert.org>
2018-06-16 00:26:23 +0200

    wip: influxdb reporter

--- test/core/runtime.ml
+++ test/core/runtime.ml
@@ -1,22 +1,76 @@
 (*************)
-(* Reporters *)
+(* influxdb line protocol reporter *)
+(* from https://docs.influxdata.com/influxdb/v1.5/write_protocols/line_protocol_reference/ *)
+(* example line: weather,location=us-midwest temperature=82 1465839830100400200 *)
 (*************)
+
+open Astring
+
+let avoid_keyword =
+  let keywords = String.Set.of_list [
+    "ALL" ; "ALTER" ; "ANY" ; "AS" ; "ASC" ; "BEGIN" ;
+    "BY" ; "CREATE" ; "CONTINUOUS" ; "DATABASE" ; "DATABASES" ; "DEFAULT" ;
+    "DELETE" ; "DESC" ; "DESTINATIONS" ; "DIAGNOSTICS" ; "DISTINCT" ; "DROP" ;
+    "DURATION" ; "END" ; "EVERY" ; "EXPLAIN" ; "FIELD" ; "FOR" ;
+    "FROM" ; "GRANT" ; "GRANTS" ; "GROUP" ; "GROUPS" ; "IN" ;
+    "INF" ; "INSERT" ; "INTO" ; "KEY" ; "KEYS" ; "KILL" ;
+    "LIMIT" ; "SHOW" ; "MEASUREMENT" ; "MEASUREMENTS" ; "NAME" ; "OFFSET" ;
+    "ON" ; "ORDER" ; "PASSWORD" ; "POLICY" ; "POLICIES" ; "PRIVILEGES" ;
+    "QUERIES" ; "QUERY" ; "READ" ; "REPLICATION" ; "RESAMPLE" ; "RETENTION" ;
+    "REVOKE" ; "SELECT" ; "SERIES" ; "SET" ; "SHARD" ; "SHARDS" ;
+    "SLIMIT" ; "SOFFSET" ; "STATS" ; "SUBSCRIPTION" ; "SUBSCRIPTIONS" ; "TAG" ;
+    "TO" ; "USER" ; "USERS" ; "VALUES" ; "WHERE" ; "WITH" ; "WRITE"
+  ] in
+  (fun m ->
+     if String.(Set.mem (Ascii.uppercase m) keywords) then
+       "o" ^ m
+     else
+       m)
+
+let escape =
+  List.fold_right (fun e m' -> String.(concat ~sep:("\\" ^ e) (cuts ~sep:e m')))
+
+let escape_measurement m =
+  escape  [ "," ; " " ] (avoid_keyword m)
+
+let escape_name m =
+  escape [ "," ; "=" ; " " ] (avoid_keyword m)
 
 let set_simple_reporter () =
   let start = Mtime_clock.now () in
   let report ~tags ~data ~over src k =
-    let data_fields = Metrics.Data.fields data in
-    let name = Metrics.Src.name src in
-    let pp_field ppf f =
-      Fmt.(pair ~sep:(unit "=") Metrics.pp_key Metrics.pp_value) ppf (f, f)
+    let open Metrics in
+    let data_fields = Data.fields data in
+    let name = escape_measurement (Src.name src) in
+    let pp_val (str : string Fmt.t) ppf f =
+      match value f with
+      | V (String, s) -> str ppf s
+      | V (Int, i) -> Fmt.pf ppf "%di" i
+      | V (Int32, i32) -> Fmt.pf ppf "%ldi" i32
+      | V (Int64, i64) -> Fmt.pf ppf "%Ldi" i64
+      | V (Uint, u) -> Fmt.pf ppf "%ui" u
+      | V (Uint32, u32) -> Fmt.pf ppf "%lui" u32
+      | V (Uint64, u64) -> Fmt.pf ppf "%Lui" u64
+      | _ -> pp_value ppf f
     in
-    let pp = Fmt.(list ~sep:(unit ",") pp_field) in
-    let timestamp = match Metrics.Data.timestamp data with
+    let pp_field_str ppf s = Fmt.pf ppf "%S" s in
+    let pp_field ppf f =
+      Fmt.(pair ~sep:(unit "=") string (pp_val pp_field_str)) ppf
+        (escape_name (key f), f)
+    in
+    let pp_fields = Fmt.(list ~sep:(unit ",") pp_field) in
+    let pp_tag_str ppf s = Fmt.string ppf (escape_name s) in
+    let pp_tag ppf f =
+      Fmt.(pair ~sep:(unit "=") string (pp_val pp_tag_str)) ppf
+        (escape_name (key f), f)
+    in
+    let pp_tags = Fmt.(list ~sep:(unit ",") pp_tag) in
+    let timestamp = match Data.timestamp data with
       | Some ts -> ts
       | None    -> Fmt.to_to_string Mtime.Span.pp
                      (Mtime.span start (Mtime_clock.now ()))
     in
-    Fmt.pr "%s %a %a %s\n%!" name pp tags pp data_fields timestamp;
+    Fmt.pr "%s,%a %a %s\n%!" name pp_tags tags pp_fields data_fields timestamp;
     over ();
     k ()
   in
@@ -100,7 +154,7 @@ let rec alloc n =
         | 0 -> ["hallo"]
         | n -> string_of_int n :: alloc (pred n)
       in
-      alloc n
+      List.init n alloc
     in
     Hashtbl.add mem n r ;
     r
